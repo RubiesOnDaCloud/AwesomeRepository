11a9c0e68054ec3fbf55f1fb4f2e5aef810c66d3
sarahkami <sarahkami@users.noreply.github.com>
2015-02-17 20:49:45 +0100

    Create 06_99BottlesOfBeer.rb

---
+++ 06_99BottlesOfBeer.rb
@@ -0,0 +1,16 @@
+beer = 99
+
+puts 'lets sing a a song :)'
+
+while beer >= 1
+	puts beer.to_s + ' bottles of beer on the wall, ' + beer.to_s + ' bottles of beer.'
+	beer= beer - 1
+	if beer != 0
+		puts 'Take one down and pass it around, ' + beer.to_s + ' bottles of beer on the wall.'
+	else
+		puts 'Take one down and pass it around, ' + 'no more bottles of beer on the wall.'
+	end
+end
+beer = 99
+puts 'No more bottles of beer on the wall, no more bottles of beer.'
+puts 'Go to the store and buy some more, ' + beer.to_s + ' bottles of beer on the wall.'
