a00ed9b1fa8ab36cd9415e403a30897632f0906e
sarahkami <sarahkami@users.noreply.github.com>
2015-05-02 22:29:37 +0200

    Update 06_99BottlesOfBeer.rb
    fixed some mistakes

--- 06_99BottlesOfBeer.rb
+++ 06_99BottlesOfBeer.rb
@@ -3,10 +3,18 @@ beer = 99
 puts 'lets sing a a song :)'
 
 while beer >= 1
-	puts beer.to_s + ' bottles of beer on the wall, ' + beer.to_s + ' bottles of beer.'
+	if beer == 1
+		puts beer.to_s + ' bottle of beer on the wall, ' + beer.to_s + ' bottle of beer.'
+	else
+		puts beer.to_s + ' bottles of beer on the wall, ' + beer.to_s + ' bottles of beer.'
+	end
 	beer= beer - 1
 	if beer != 0
-		puts 'Take one down and pass it around, ' + beer.to_s + ' bottles of beer on the wall.'
+		if beer == 1 
+			puts 'Take one down and pass it around, ' + beer.to_s + ' bottle of beer on the wall.'
+		else
+			puts 'Take one down and pass it around, ' + beer.to_s + ' bottles of beer on the wall.'
+		end
 	else
 		puts 'Take one down and pass it around, ' + 'no more bottles of beer on the wall.'
 	end
